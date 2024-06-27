b5aca4540127f5cf44d1443ec912e9550dd25918
VeniGogniti <stefx@op.pl>
2024-06-27 16:08:50 +0200

    adding works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -5,19 +5,16 @@ std::string calculate(const std::string& command, int first, int second) {
     int result;
     if (command == "add") {
         result = first + second;
-    }
-    else if (command == "subtract") {
+    } else if (command == "subtract") {
         result = first - second;
-    }
-    else if (command == "multiply") {
+    } else if (command == "multiply") {
         result = first * second;
-    }
-    else if (command == "divide") {
-      if (first == 0 || second == 0) {
-        return "Division by 0";
-      } else {
-        result = first / second;
-      } 
+    } else if (command == "divide") {
+        if (first == 0 || second == 0) {
+             return "Division by 0";
+        } else {
+            result = first / second;
+        } 
     } else {
         return "Invalid data";
     }
