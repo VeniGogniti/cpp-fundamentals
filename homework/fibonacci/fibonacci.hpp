b2064388b85f09057489b913da8ae5ab0380786a
VeniGogniti <stefx@op.pl>
2024-06-28 12:06:00 +0200

    adding works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,25 @@
 #pragma once
+#include <iostream>
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int liczbaA = 0;
+    int liczbaB = 0;
+    int wynik = 0;
+    for (int i = 0; i <= sequence; i++) {
+        wynik = liczbaB + liczbaA;
+        liczbaA = liczbaB;
+        liczbaB = wynik;
+        if (wynik == 0) {
+            liczbaA = 1;
+        }
+    }
+    return wynik;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 1) {
+        return sequence;
+    } else {
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+    }
 }
