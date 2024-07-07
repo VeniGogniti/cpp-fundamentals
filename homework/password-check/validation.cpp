9698eb1bbc3252ddc9de92d54163212ea7860339
VeniGogniti <51407963+VeniGogniti@users.noreply.github.com>
2024-07-07 12:15:32 +0200

    Update validation.cpp

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -4,15 +4,15 @@ std::string getErrorMessage(ErrorCode code) {
     switch (code) {
     case ErrorCode::Ok:
         return "Ok";
-    case ErrorCode::PasswordNeedsAtLeastNineCharacters: 
+    case ErrorCode::PasswordNeedsAtLeastNineCharacters:
         return "Password needs to have at least nine characters";
-    case ErrorCode::PasswordNeedsAtLeastOneNumber: 
+    case ErrorCode::PasswordNeedsAtLeastOneNumber:
         return "Password needs to have at least one number";
-    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter: 
+    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
         return "Password needs to have at least one special character";
-    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter: 
+    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
         return "Password needs to have at least one uppercase letter";
-    case ErrorCode::PasswordsDoNotMatch: 
+
         return "Passwords do not match";
     default: return "Error";
     }
