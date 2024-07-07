140983e00cff4686703f9d51ccc48f003d187de0
VeniGogniti <51407963+VeniGogniti@users.noreply.github.com>
2024-07-07 12:02:04 +0200

    Update validation.cpp

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -2,12 +2,18 @@
 
 std::string getErrorMessage(ErrorCode code) {
     switch (code) {
-        case ErrorCode::Ok: return "Ok";
-        case ErrorCode::PasswordNeedsAtLeastNineCharacters: return "Password needs to have at least nine characters";
-        case ErrorCode::PasswordNeedsAtLeastOneNumber: return "Password needs to have at least one number";
-        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter: return "Password needs to have at least one special character";
-        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter: return "Password needs to have at least one uppercase letter";
-        case ErrorCode::PasswordsDoNotMatch: return "Passwords do not match";
+        case ErrorCode::Ok: 
+            return "Ok";
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters: 
+            return "Password needs to have at least nine characters";
+        case ErrorCode::PasswordNeedsAtLeastOneNumber: 
+            return "Password needs to have at least one number";
+        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter: 
+            return "Password needs to have at least one special character";
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter: 
+            return "Password needs to have at least one uppercase letter";
+        case ErrorCode::PasswordsDoNotMatch: 
+            return "Passwords do not match";
         default: return "Error";
     }
 }
@@ -33,8 +39,7 @@ ErrorCode checkPasswordRules(std::string pwd) {
                 return ErrorCode::PasswordNeedsAtLeastOneNumber;
             }
         }
-        return ErrorCode::Ok;
-        
+        return ErrorCode::Ok;        
     } else if (pwd.size() < 9) {
         return ErrorCode::PasswordNeedsAtLeastNineCharacters;
     }
