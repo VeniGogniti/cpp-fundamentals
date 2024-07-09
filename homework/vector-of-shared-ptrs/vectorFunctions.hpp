7cea37ca3a87a4ed93a5468cd2f2931f968d7d15
VeniGogniti <51407963+VeniGogniti@users.noreply.github.com>
2024-07-09 09:56:22 +0200

    Update vectorFunctions.hpp

--- homework/vector-of-shared-ptrs/vectorFunctions.hpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -5,13 +5,13 @@
 #ifndef VECTOROFSHAREDPOINTERS_VECTORFUNCTIONS_HPP
 #define VECTOROFSHAREDPOINTERS_VECTORFUNCTIONS_HPP
 #include <iostream>
-#include <vector>
 #include <memory>
+#include <vector>
 
 std::vector<std::shared_ptr<int>> generate(int count);
 void print(std::vector<std::shared_ptr<int>> printVector);
 void add10(std::vector<std::shared_ptr<int>> addVector);
-void sub10(int * const subVector);
+void sub10(int* const subVector);
 void sub10(std::vector<std::shared_ptr<int>> przeciazenieVector);
 
 #endif //VECTOROFSHAREDPOINTERS_VECTORFUNCTIONS_HPP
